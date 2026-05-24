40d1a4fbe365d00823392d0930861311baf86385
lourdes222 <lourdes.munoz666222@gmail.com>
2026-05-24 19:30:29 -0300

    inicio de sesión

--- script.js
+++ script.js
@@ -12,3 +12,21 @@ function buscar(){
         alert("No se encontró la sección buscada. Por favor, intente con 'Inicio', 'Viajes', 'Estadías' o 'Traslados'.");
     }
 }
+let carrito=[];
+function agregarAlCarrito(nombre, precio){
+    carrito.push({nombre, precio});
+    alert("¡Sumaste"+nombre+" al carrito!");
+    console.log("Tu carrito actual: ", carrito);
+}
+console.log("el archivo cargó")
+
+function abrirLogin(){
+    document.getElementById('modal-login').style.display='block';
+}
+function cerrarLogin(){
+    document.getElementById('modal-login').style.display='none'
+}
+function entrar(){
+    alert("Iniciando sesión...");
+    cerrarLogin();
+}
