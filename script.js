671041cf30ba596443376e1526a75933f07527ad
tizi <140276985+qfoge@users.noreply.github.com>
2026-05-28 16:40:16 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,59 @@
 function buscar(){
-    let texto=document.getElementById("busqueda").value.toLowerCase();
-    if(texto=="inicio"){
-        document.getElementById("inicio").scrollIntoView();
-    }   else if(texto=="viajes"){
-        document.getElementById("viajes").scrollIntoView();
-    }else if(texto==="estadias"){
-        document.getElementById("estadias").scrollIntoView();
-    }else if(texto==="traslados"){
-        document.getElementById("traslados").scrollIntoView();
+
+    let texto = document
+        .getElementById("busqueda")
+        .value
+        .toLowerCase();
+
+    if(texto === "inicio"){
+
+        window.location.href = "index.html";
+
+    }else if(texto === "viajes"){
+
+        window.location.href = "viajes.html";
+
+    }else if(texto === "estadias"){
+
+        window.location.href = "estadias.html";
+
+    }else if(texto === "traslados"){
+
+        window.location.href = "traslados.html";
+
     }else{
-        alert("No se encontró la sección buscada. Por favor, intente con 'Inicio', 'Viajes', 'Estadías' o 'Traslados'.");
+
+        alert("No se encontró la sección 😭");
+
     }
 }
-let carrito=[];
+
+let carrito = [];
+
 function agregarAlCarrito(nombre, precio){
+
     carrito.push({nombre, precio});
-    alert("¡Sumaste"+nombre+" al carrito!");
-    console.log("Tu carrito actual: ", carrito);
+
+    alert("¡Sumaste " + nombre + " al carrito!");
+
+    console.log(carrito);
 }
-console.log("el archivo cargó")
 
 function abrirLogin(){
-    document.getElementById('modal-login').style.display='block';
+
+    document.getElementById("modal-login")
+        .style.display = "block";
 }
+
 function cerrarLogin(){
-    document.getElementById('modal-login').style.display='none'
+
+    document.getElementById("modal-login")
+        .style.display = "none";
 }
+
 function entrar(){
+
     alert("Iniciando sesión...");
+
     cerrarLogin();
 }
